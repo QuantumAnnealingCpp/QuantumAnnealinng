fdf9aaba48ded93c4f559afc85b20a1cf301f1de
pawekuk <paffcio314@gmail.com>
2018-04-08 15:07:34 +0200

    init commit

---
+++ QuantumAnnealing/QAS.cpp
@@ -0,0 +1,242 @@
+#include "stdafx.h"
+#include "QAS.h"
+
+
+QAS::QAS()
+{
+
+}
+
+QAS::QAS(int n, int m)
+{
+	N = n;
+	M = m;
+	spinsInit();
+	JInit();
+
+}
+
+QAS::QAS(int n, int m, double t, double gammaa, int nt, int snapnt, double kb )
+{
+	N = n;
+	M = m;
+	T = t;
+	gamma = gammaa;
+	NT = nt;
+	snapNT = snapnt;
+	kB = kb;
+	beta = 1 / (T * kB);
+	spinsInit();
+	JInit();
+}
+
+QAS::QAS(int n, int m, double t, double gammaa, int nt, int snapnt, double kb, int gsteps, int tsteps)
+{
+	N = n;
+	M = m;
+	T = t;
+	gamma = gammaa;
+	NT = nt;
+	snapNT = snapnt;
+	kB = kb;
+	beta = 1 / (T * kB);
+	spinsInit();
+	JInit();
+
+	Gsteps = gsteps;
+	Tsteps = tsteps;
+}
+
+void QAS::flipSpin() {
+	//choose random spin on NxM grid
+	int tmpi, tmpk;
+	//srand(time(NULL));
+	tmpi = rand() % N;
+	tmpk = rand() % M;
+	
+	//calculates the energy difference for the Metropolis algorithm
+	double deltaEnergy = energyDifferenceNN(tmpi, tmpk);
+
+	//Metropolis algorithm
+	if (deltaEnergy < 0)
+	{
+		//cout << "O";
+		spins[tmpi][tmpk] *= -1; //* spins[tmpi][tmpk];
+		//keeping track of magnetization
+		magnetization += 2 * spins[tmpi][tmpk];
+	} else 
+	{
+		double boltzman = exp(- deltaEnergy * beta);
+		double uniform = rand() /(double) RAND_MAX;
+		if (uniform < boltzman) {
+			spins[tmpi][tmpk] *= -1;
+			//cout << "_";
+			//keeping track of magnetization
+			magnetization += 2 * spins[tmpi][tmpk];
+		}
+	}
+}
+
+void QAS::printMagnetization()
+{
+	cout << "Magnetization = " << magnetization/(M*N) << endl;
+}
+
+void QAS::printMagAvgTime()
+{
+	cout << "MagAvgTime = " << magAvgTime << endl;
+}
+
+double QAS::energyDifferenceNN(int n, int m)
+{
+	if(n != 0 && m != 0)
+		return 2.0 *(spins[n][m] * (J[n][n - 1] * spins[n - 1][m] + J[n][(n + 1) % N] *
+			spins[(n + 1) % N][m]) /( (double)M) + 0.5 / beta * log(1.0 / tanh(beta*gamma / M)) *
+			spins[n][m] * (spins[n][m - 1] + spins[n][(m + 1) % M]));
+	else if(n == 0 && m != 0)
+		return 2.0 * (spins[n][m] * (J[n][N - 1] * spins[N - 1][m] + J[n][(n + 1) % N] *
+			spins[(n + 1) % N][m]) / ((double)M) + 0.5 / beta * log(1.0 / tanh(beta*gamma / M)) *
+			spins[n][m] * (spins[n][m - 1] + spins[n][(m + 1) % M]));
+	else if(m == 0 && n != 0)
+		return 2.0 * (spins[n][m] * (J[n][n - 1] * spins[n - 1][m] + J[n][(n + 1) % N] *
+			spins[(n + 1) % N][m]) / ((double)M) + 0.5 / beta * log(1.0 / tanh(beta*gamma / M)) *
+			spins[n][m] * (spins[n][M - 1] + spins[n][(m + 1) % M]));
+	else if ( n == 0 && m == 0)
+		return 2.0 * (spins[n][m] * (J[n][N - 1] * spins[N - 1][m] + J[n][(n + 1) % N] *
+			spins[(n + 1) % N][m]) / ((double)M) + 0.5 / beta * log(1.0 / tanh(beta*gamma / M)) *
+			spins[n][m] * (spins[n][M - 1] + spins[n][(m + 1) % M]));
+}
+
+void QAS::simulation() {
+	spinsInit();
+	magHistory.clear();
+	for (size_t i = 0; i < NT; i++)
+	{
+		flipSpin();
+		if (i % snapNT == 0) {
+			magHistory.push_back(magnetization / (N*M));
+		}
+	}
+	magAvgOverTime();
+}
+
+double QAS::simulation(double t, double gammaa) {
+	T = t;
+	gamma = gammaa;
+	spinsInit();
+	magHistory.clear();
+	for (size_t i = 0; i < NT; i++)
+	{
+		flipSpin();
+		if (i % snapNT == 0) {
+			magHistory.push_back(magnetization / (N*M));
+		}
+	}
+	magAvgOverTime();
+	return magAvgTime;
+}
+
+void QAS::spinsInit() {
+	srand(time(NULL));
+	magnetization = 0;
+	this->spins.resize(this->N, vector<int>(this->M, 0));
+	for (size_t i = 0; i < N; i++)
+	{
+		for (size_t j = 0; j < M; j++)
+		{
+			spins[i][j] = 2 *( rand() % 2 )- 1;
+			magnetization += spins[i][j];
+		}
+	}
+}
+
+void QAS::JInit()
+{
+	this->J.resize(this->N, vector<int>(this->N, 0));
+	for (size_t i = 0; i < N - 1; i++)
+	{
+		J[i][i + 1] = 1;
+		J[i + 1][i] = 1;
+	}
+	J[0][N - 1] = 1;
+	J[N - 1][0] = 1;
+}
+
+void QAS::printSpins()
+{
+	cout << endl;
+	for (size_t i = 0; i < N; i++)
+	{
+		for (size_t j = 0; j < M; j++)
+		{
+			cout << spins[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+void QAS::printJ()
+{
+	for (size_t i = 0; i < N; i++)
+	{
+		for (size_t j = 0; j < N; j++)
+		{
+			cout << J[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+QAS::~QAS()
+{
+}
+
+void QAS::magAvgOverTime()
+{
+	double integral = 0;
+	int lenEnd = magHistory.size();
+	int len0 = (int)(magHistory.size()/2);
+	for (size_t i = len0; i < lenEnd; i++)
+	{
+		integral += magHistory[i];
+	}
+	magAvgTime = integral / (lenEnd - len0);
+		
+}
+
+void QAS::simulationGammaRange() {
+	gammaHistory.clear();
+	gammaHistory.resize(2, vector<double>(this->Gsteps, 0));
+	
+	vector<double> gammaRange;
+	gammaRange.resize(Gsteps, 0);
+	double tmp = gamma;
+	double dgamma = gamma / ((double)Gsteps);
+	
+	for (size_t i = 0; i < Gsteps; i++)
+	{
+		gammaRange[i] = tmp;
+		tmp -= dgamma;
+	
+	}
+	double magTmp = 0;
+	for (size_t i = 0; i < Gsteps; i++)
+	{
+		magTmp = simulation(T, gammaRange[i]);
+		gammaHistory[0][i] = gammaRange[i];
+		gammaHistory[1][i] = abs(magTmp);
+	}
+		
+}
+
+void QAS::printGammaHistory()
+{
+	for (size_t i = 0; i < Gsteps; i++)
+	{
+		for (size_t j = 0; j < 2; j++)
+		{
+			cout << gammaHistory[j][i] << "\t";
+		}
+		cout << endl;
+	}
+}
---
+++ QuantumAnnealing/QAS.h
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef _QAS_H_ 
+#define _QAS_H_
+#include<vector>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+class QAS
+{
+public:
+	vector< vector<int> > spins; // 2D system of spins
+	vector< vector<int> > J;
+	vector<double> magHistory;
+	vector<vector<double>> gammaHistory;
+	double magnetization;
+	double T; //thermodynamical temperature
+	double gamma; //transverse field strenght factor
+	double kB ; //Boltzman constant
+	double beta;
+	double magAvgTime;
+	int N; //N quantum spins
+	int M; //additional dimension of spins
+	int NT; //liczba kroków czasowych, w których losowany jest jeden spin
+	int snapNT; //a value of magnetization is saved every 1000 steps of the simulation
+	int Gsteps; //number of evaluation points of gamma for the | <s> | = f(gamma) plot
+	int Tsteps; //number of times temperature is decreased during annealing
+	void spinsInit(); //initialization of grid of spins
+	void JInit(); //initialization of grid of interactions
+	void printSpins();
+	void printJ();
+	QAS();
+	QAS(int n, int m);
+	QAS(int n, int m, double t, double gammaa, int nt, int snapnt, double kb);
+	QAS(int n, int m, double t, double gammaa, int nt, int snapnt, double kb, int gsteps, int tsteps);
+	void flipSpin(); //decides whether to change the state of a random spin according to Metropolis algorithm
+	void printMagnetization();
+	void printMagAvgTime();
+	void simulation();
+	double simulation(double T, double gamma);
+	//function uses Monthe Carlo method for searching the minimum of the effective hamiltonian
+	double energyDifferenceNN(int n, int m); //calculates the energy difference taking into consideration only the nearest neighbours
+	~QAS();
+	void magAvgOverTime();
+	void simulationGammaRange();
+	void printGammaHistory();
+};
+
+#endif 
